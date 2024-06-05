040d06e7107930c5f97a3b813dce1d44313a913d
System Administrator <root@Naiems-MacBook-Pro.local>
2024-06-05 18:19:00 +0600

    all user show and make admin

--- index.js
+++ index.js
@@ -58,6 +58,25 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userssCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userssCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
     // tests
     app.get("/tests", async (req, res) => {
       const result = await testsCollection.find().toArray();
