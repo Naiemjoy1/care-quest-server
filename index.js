899dc8708abf31c7f420a7cec64122f919687f73
System Administrator <root@Naiems-MacBook-Pro.local>
2024-06-06 13:49:11 +0600

    add test

--- index.js
+++ index.js
@@ -164,6 +164,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/tests", async (req, res) => {
+      const item = req.body;
+      const result = await testsCollection.insertOne(item);
+      res.send(result);
+    });
+
     // reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
