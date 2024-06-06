1e62434a6f52371d095ea39d47fbf8d980a39a53
System Administrator <root@Naiems-MacBook-Pro.local>
2024-06-06 12:17:39 +0600

    show all test and delete

--- index.js
+++ index.js
@@ -157,6 +157,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/tests/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await testsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
